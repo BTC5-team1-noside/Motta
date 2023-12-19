56451c47dacf40e53bf1c4c0c1e5e3cee65bd416
gwony <cong129@gmail.com>
2023-12-19 13:56:08 +0900

    student-start

---
+++ client/student/lib/models/belongings.dart
@@ -0,0 +1,33 @@
+import "package:flutter/material.dart";
+
+class Date {
+  Date({required this.dateTime});
+  final DateTime dateTime;
+
+  @override
+  String toString() {
+    return dateTime.toString();
+  }
+}
+
+class Subject {
+  Subject({
+    required this.period,
+    required this.subject,
+    required this.belongings,
+  });
+  final String period;
+  final String subject;
+  final List<String> belongings;
+}
+
+class DayBelonging {
+  DayBelonging(
+      {required this.selectedDate,
+      required this.subjects,
+      required this.items});
+
+  final Date selectedDate;
+  final List<Subject> subjects;
+  final List<String> items;
+}
